e1c157be29b659ccb5a7db9f80e56e0035f00d97
Shingo Nishimoto <sn-0121@g.ecc.u-tokyo.ac.jp>
2020-05-23 16:29:01 +0900

    initial version of thermal analize code

---
+++ Thermal_analysis.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+#include<assert.h>
+    
+void explicit_method();
+void inplicit_method();
+
+int	main(int	argc,	char	const	*argv[]){
+    double t;
+    double *Ti,*Tj,*Ci,*Cij; //ここはどのデータ構造にするか要検討
+    double sigma = 5.669e-8; //[W/(m^2K^4)]
+    double *Rij; //形態係数
+    int N=10,T=60*60*24; //N:ノード数，T:伝搬時間[s]
+
+
+    return	0;
+}
+
+void explicit_method(){
+
+}
